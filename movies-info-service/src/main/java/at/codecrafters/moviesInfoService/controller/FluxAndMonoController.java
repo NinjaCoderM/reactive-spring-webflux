6050ad0275e0819d1e867025673a0a8cc1bc94a2
wienm <martin.scheibelreiter@chello.at>
2025-03-06 20:42:12 +0100

    Streaming Endpoint MediaType.TEXT_EVENT_STREAM_VALUE

--- movies-info-service/src/main/java/at/codecrafters/moviesInfoService/controller/FluxAndMonoController.java
+++ movies-info-service/src/main/java/at/codecrafters/moviesInfoService/controller/FluxAndMonoController.java
@@ -1,9 +1,12 @@
 package at.codecrafters.moviesInfoService.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import java.time.Duration;
+
 
 @RestController
 @RequestMapping(path="/flux")
@@ -13,6 +16,12 @@ public class FluxAndMonoController {
     public Flux<String> getWelcomeMessage() {
         // Test with curl http://localhost:8080/flux/welcome
         return Flux.just("Welcome to Flux And Mono! ", "Yes ", "Year ", "For Real ");
+    }
+
+    @GetMapping(path = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Integer> getStream() {
+        // Test with curl http://localhost:8080/flux/stream
+        return Flux.range(1, 100).delayElements(Duration.ofSeconds(1)).log();
     }
 
 }
