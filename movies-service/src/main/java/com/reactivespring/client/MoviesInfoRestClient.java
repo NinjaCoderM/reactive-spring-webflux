9145e421fd1b57b052a27d3334edc8ffb9c75d5d
wienm <martin.scheibelreiter@chello.at>
2025-03-14 09:41:41 +0100

    streaming Client using WebClient

--- movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
@@ -1,6 +1,7 @@
 package com.reactivespring.client;
 
 import com.reactivespring.config.RetryUtil;
+import com.reactivespring.domain.Movie;
 import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.exception.MoviesInfoClientException;
 import com.reactivespring.exception.MoviesInfoServerException;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
 
@@ -46,6 +48,28 @@ public class MoviesInfoRestClient {
                             .flatMap(responseMessage -> Mono.error(new MoviesInfoServerException("ServerException in MoviesInfoService: "+responseMessage)));
                 })
                 .bodyToMono(MovieInfo.class)
+                .retryWhen(RetryUtil.retrySpec(
+                        MoviesInfoServerException.class,
+                        retrySignal -> new MoviesInfoServerException("Max retries reached: " +
+                                retrySignal.totalRetries() + " Cause: " + retrySignal.failure().getMessage())
+                ));
+    }
+
+    public Flux<MovieInfo> retrieveMovieInfoStream() {
+        var url = movieInfoUrl.concat("/stream");
+        return webClient.get().uri(url)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
+                    log.info("Status code is: {}", clientResponse.statusCode().value());
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(responseMessage -> Mono.error(new MoviesInfoClientException(responseMessage, clientResponse.statusCode().value() )));
+                })
+                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> {
+                    log.info("Status code is: {}", clientResponse.statusCode().value());
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(responseMessage -> Mono.error(new MoviesInfoServerException("ServerException in MoviesInfoService: "+responseMessage)));
+                })
+                .bodyToFlux(MovieInfo.class)
                 .retryWhen(RetryUtil.retrySpec(
                         MoviesInfoServerException.class,
                         retrySignal -> new MoviesInfoServerException("Max retries reached: " +
--- movies-service/src/main/java/com/reactivespring/controller/MoviesController.java
+++ movies-service/src/main/java/com/reactivespring/controller/MoviesController.java
@@ -3,10 +3,13 @@ package com.reactivespring.controller;
 import com.reactivespring.client.MoviesInfoRestClient;
 import com.reactivespring.client.ReviewsRestClient;
 import com.reactivespring.domain.Movie;
+import com.reactivespring.domain.MovieInfo;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -29,5 +32,10 @@ public class MoviesController {
         return movieInfoMono.flatMap(movieInfo ->
              reviews.map(reviewList -> new Movie(movieInfo, reviewList))
         );
+    }
+
+    @GetMapping(value = "/stream", produces = MediaType.APPLICATION_NDJSON_VALUE)
+    public Flux<MovieInfo> retrieveMovieInfosStream() {
+        return moviesInfoRestClient.retrieveMovieInfoStream();
     }
 }
